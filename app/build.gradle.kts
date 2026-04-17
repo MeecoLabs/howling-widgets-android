be83f04f25b8aaf5b646a2982263f40a01c8d92c
Dustin Steiner <dustin.steiner@ik.me>
2026-04-17 09:12:46 +0100

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "eu.meecolabs.howlingwidgets"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = SimpleDateFormat("yyyy.MM.dd").format(Date())
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
